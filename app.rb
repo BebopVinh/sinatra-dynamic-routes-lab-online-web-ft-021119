6800bc673564b2f25df0596e1bba211ac444eff4
Vinh Do <vinhdo07@gmail.com>
2019-04-02 03:16:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,4 +3,17 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+
+  get '/square/:number' do
+    params[:number].to_i ** 2
+  end
+
+  get '/say/:number/:phrase' do 
+    params[:number].to_i.times do 
+      params[:phrase]
+    end
+  end
 end
