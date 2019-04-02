7312ca3a3e60aeaf97262d651657887f606630c5
Vinh Do <vinhdo07@gmail.com>
2019-04-02 03:23:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     x = params[:number].to_i.times do
-      params[:phrase]
+      "#{params[:phrase]}"
     end
     binding.pry
   end
